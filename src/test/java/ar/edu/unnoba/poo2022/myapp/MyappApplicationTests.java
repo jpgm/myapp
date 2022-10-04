12c94c487f27f946c518859b17d10a676bb69e7e
Juan Pablo Gutierrez <juanpablo.gutierrez@simplex.software>
2022-10-04 08:42:21 -0300

    commit inicial

---
+++ src/test/java/ar/edu/unnoba/poo2022/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2022.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
